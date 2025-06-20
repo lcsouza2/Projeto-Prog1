8aaf7ba749adc722f65584be055e23f573f87ae2
Luiz Carlos Souza Garcia <lcsouzagarcia@gmail.com>
2025-06-20 20:16:17 -0300

    feat: function criarRelatorio that saves a report on a .txt file

--- main.c
+++ main.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-void mostrarMenuPrincipal();
-void MenuExercicios();
-void MenuRelatorios();
+#define CAMINHO_ARQUIVO_RELATORIOS "./relatorios.txt"
+
+int menuPrincipal();
+int menuExercicios();
+int menuRelatorios();
+
+char verRelatorios();
+void criarRelatorio();
+
+struct ExercicioRelatorio{
+    char nome[101];
+    char series[20];
+    char peso[10];
+};
+
 
 int main() {
-    int opcaoPrincipal;
-
+    int opcao;
     do {
-        mostrarMenuPrincipal();
-        switch (opcaoPrincipal) {
+        opcao = menuPrincipal();
+        switch (opcao) {
         case 1:
-            mostrarMenuExercicios();
+            menuExercicios();
             break;
         case 2:
-            MostrarMenuRelatorio();
-        default:
-            break;
+            menuRelatorios();
         }
-    } while (opcaoPrincipal != 3)
+    } while (opcao != 3);
 
 }
 
 
-void mostrarMenuPrincipal() {
+int menuPrincipal() {
+    int opcaoPrincipal;
     printf("\n=== MENU PRINCIPAL ===\n");
     printf("1. Exercicios\n");
     printf("2. Relatorios\n");
@@ -31,9 +43,10 @@ void mostrarMenuPrincipal() {
     printf("=====================\n");
     printf("Escolha uma opcao: ");
     scanf("%d", &opcaoPrincipal);
+    return opcaoPrincipal;
 }
 
-int MenuExercicios() {
+int menuExercicios() {
     int opcaoExercicio;
 
     printf("\n=== EXERCICIOS ===\n");
@@ -61,16 +74,15 @@ int MenuExercicios() {
         //funcao
         break;
     case 5:
-        MostrarMenuPrincipal();
+        return 0;
         break;
     default:
-        printf("Opcao Invalida!")
-        return
+        printf("Opcao Invalida!");
         break;
     }
 }
 
-int MenuRelatorios() {
+int menuRelatorios() {
     int opcaoRelatorio;
 
     printf("\n=== RELATORIOS ===\n");
@@ -88,18 +100,91 @@ int MenuRelatorios() {
         //função
         break;
     case 2:
-        //função
+        criarRelatorio();
         break;
     case 3:
         //funcao
         break;
     case 4:
-        MostrarMenuPrincipal();
-        
+        return 0;
         break;
     default:
         printf("Opcao Invalida!");
-        return;
         break;
+    }
+}
+
+void criarRelatorio() {
+    FILE *arquivo;
+    char temp[101];
+    char dataRelatorio[12];
+
+    arquivo = fopen(CAMINHO_ARQUIVO_RELATORIOS, "a");
+
+    if (!arquivo) {
+        printf("Nao foi possível abrir o arquivo de dados!");
+        return;
+    }
+
+    printf("=====================\n");
+    printf("Digite a data do relatório ou 'cancelar' para voltar ao menu: ");
+    
+    scanf("%s", dataRelatorio);
+    
+    if (strcmp(dataRelatorio, "cancelar") != 0) { 
+        fprintf(arquivo, "[RELATORIO]\n");
+        
+        fprintf(arquivo, "DATA=%s\n", dataRelatorio);
+    } else {
+        return;
+    }
+
+    do {
+        struct ExercicioRelatorio exercicioTemp;
+
+        printf("Adicione um exercicio ou digite 'sair' para encerrar o relatorio!\n");
+        
+        printf("Digite o nome do exercicio: ");
+        scanf("%s", temp);
+        if (strcmp(temp, "sair") == 0) {
+            break;
+        } else {
+            strcpy(exercicioTemp.nome, temp);
+        }
+
+        printf("Digite o peso utilizado: ");
+        scanf("%s", temp);
+        if (strcmp(temp, "sair") == 0) {
+            break;
+        } else {
+            strcpy(exercicioTemp.peso, temp);
+        }
+
+        printf("Digite a quantidade de series no formato seriesXrepeticoes, ex. 3x12: ");
+        scanf("%s", temp);
+        if (strcmp(temp, "sair") == 0) {
+            break;
+        } else {
+            strcpy(exercicioTemp.series, temp);
+        }
+
+        printf("=====================\n");
+        
+        fprintf(arquivo, "EXERCICIO=%s;%s;%s\n", exercicioTemp.nome, exercicioTemp.peso, exercicioTemp.series);
+        
+    } while (1);
+    
+    fprintf(arquivo, "[/RELATORIO]\n\n");
+    fclose(arquivo);
+}
+
+char verRelatorios() {
+    FILE *arquivo;
+
+    arquivo = fopen(CAMINHO_ARQUIVO_RELATORIOS, "r");
+    char linha[256];
+
+    while (fgets(linha, sizeof(linha), arquivo)) {
+
     }
 }
